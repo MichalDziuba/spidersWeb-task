40cfc4ca14377b1cdf034821944b2a341aa0ad51
Michał <michaldziuba.praca@gmail.com>
2023-04-23 20:03:51 +0200

    update

--- next.config.js
+++ next.config.js
@@ -15,18 +15,5 @@ const nextConfig = {
       ],
     },
 };
-// module.exports = {
-//   nextConfig,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "loremflickr.com",
-//         port: "",
-//         pathname: "/640/480/**",
-//       },
-//     ],
-//   },
-// };
 
 module.exports = nextConfig
